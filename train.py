4c8519ef7d285f76086fa25f43da20a37c993e76
Mach-web <mkanderson771@gmail.com>
2024-01-18 13:10:34 +0300

    Train a new network on a data set with train.py.

--- train.py
+++ train.py
@@ -19,15 +19,14 @@ data_dir = args.data_dir
 # %matplotlib inline
 # %config InlineBackend.figure_format = "retina"
 def main():
-    print(prepare_data(project_path, data_dir))
-    # save_model()
+    save_model()
 
 
 def train_model():
     dataloaders = prepare_data(project_path, data_dir)
     print("Data loading finished")
 
-    model = build_model(model_name)
+    model = build_model(model_name, args.hidden_units)
     print("Model built successfully")
     # choose either gpu or cpu
     gpu = args.gpu
@@ -45,7 +44,6 @@ def train_model():
     criterion = nn.NLLLoss()
     optimizer = optim.Adam(model.classifier.parameters(), lr = args.learning_rate)
     model.to(processor)
-
     # Training the model
     epochs = args.epochs
     train_losses = []
@@ -56,20 +54,22 @@ def train_model():
         train_loss = 0
         valid_loss = 0
         valid_accuracy = 0
-
-        for images, labels in dataloaders['train_loaders']:
+   
+        for images, labels in dataloaders['train_loader']:
             images, labels = images.to(processor), labels.to(processor)
             optimizer.zero_grad()
+         
+#             logps = model(images)
             logps = model.forward(images)
             loss = criterion(logps, labels)
             train_loss += loss.item()
-
+        
             loss.backward()
             optimizer.step()
 
         else:
             model.eval()
-            for images, labels in dataloaders['valid_loaders']:
+            for images, labels in dataloaders['valid_loader']:
                 images, labels = images.to(processor), labels.to(processor)
                 with torch.no_grad():
                     logps = model.forward(images)
@@ -78,7 +78,7 @@ def train_model():
 
                     ps = torch.exp(logps)
                     top_p, top_k = ps.topk(1, dim = 1)
-                    equals = top_k == labels.view(*top_k)
+                    equals = top_k.view(-1) == labels
                     accuracy = torch.mean(equals.type(torch.FloatTensor)).item()
                     valid_accuracy += accuracy
 
@@ -90,30 +90,28 @@ def train_model():
             valid_losses.append(valid_loss)
             valid_accuracies.append(valid_accuracy)
 
-            print("Epoch: {}".format(e),
+            print("Epoch: {}".format(e+1),
                   "Train loss: {}".format(train_loss),
                   "Test Loss: {}".format(valid_loss),
                   "Test_accuracy: {}".format(valid_accuracy))
             model.train()
-    plt.plot(train_losses, label='Training loss');
-    plt.plot(valid_losses, label='Validation loss');
-    plt.legend(frameon=False);
-    return model
+    return model, processor
 
 def test_model():
     dataloaders = prepare_data(project_path, data_dir)
-    model = train_model()
+    model, processor = train_model()
     model.eval()
     test_accuracy = 0
     for images, labels in dataloaders['test_loader']:
+        images, labels = images.to(processor), labels.to(processor)
         with torch.no_grad():
             logps = model.forward(images)
 
             ps = torch.exp(logps)
             top_p, top_k = ps.topk(1, dim=1)
-            equals = top_k.flatten() == labels
+            equals = top_k.view(-1) == labels
             test_accuracy += torch.mean(equals.type(torch.FloatTensor))
-    print(f"The test accuracy is: {test_accuracy/len(dataloaders['test_loaders'])}")
+    print(f"The test accuracy is: {test_accuracy/len(dataloaders['test_loader'])}")
     return model
 
 def save_model():
