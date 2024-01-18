e41fbfb93bec6d1f76ac40677f1cca3beba82109
Mach-web <mkanderson771@gmail.com>
2024-01-18 13:10:00 +0300

    Gets the command line input into the scripts.

--- options.py
+++ options.py
@@ -10,13 +10,13 @@ def options_train():
     parser.add_argument("--hidden_units", type=int, default=512)
     parser.add_argument("--epochs", type=int, default=5)
     parser.add_argument("--gpu", default='True')
-    parser.add_argument("--modelName", type=str, default="checkpoint.pth")
+    parser.add_argument("--modelName", type=str, default="densenet169.pth")
     return parser.parse_args()
 
 def options_predict():
     parser = argparse.ArgumentParser()
     parser.add_argument("path_image")
-    parser.add_argument("checkpoint" )
+    parser.add_argument("checkpoint")
     parser.add_argument("--top_k", type=int, default=3)
     parser.add_argument("--category_names", type=str, default="cat_to_name.json")
     parser.add_argument("--gpu", default='True')
